d8f54239016b0cdca16b537b0ad294e10fddc919
Jana Fisdel <janafisdel@gmail.com>
2022-05-17 20:54:18 -0300

    agregado de multer

--- clase8/routes/routesProductos.js
+++ clase8/routes/routesProductos.js
@@ -1,50 +1,61 @@
 const { Router } = require("express");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
+const upload = multer({ storage });
 const routerProductos = Router();
 
 const Contenendor = require("../contenedor");
 
 const productos = new Contenendor("productos.json");
 
-
 routerProductos.get("/", async (req, res) => {
   const todos = await productos.getAll();
   return res.status(200).json(todos);
 });
 
-routerProductos.get("/:id", async(req, res)=>{
-    const id= Number(req.params.id)
-    const prodId = await productos.getByID(id)
-    return res.status(200).json(prodId)
-})
+routerProductos.get("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const prodId = await productos.getByID(id);
+  return res.status(200).json(prodId);
+});
 
-routerProductos.post("/" , async(req, res)=>{
-    const {body} = req
-    productos.save(body)
-    return res.status(200).json(body)
-})
+routerProductos.post("/", upload.single("thumbnail"), async (req, res) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).json({ error: "Por favor subir un archivo" });
+  }
+  
+  const { body } = req;
+  productos.save({ ...body, thumbnail: file.path });
+  return res.status(200).json({ ...body, thumbnail: file.path });
+});
 
-routerProductos.put("/:id", async(req, res)=>{
-    const id = Number(req.params.id)
-    const todos = await productos.getAll()
-    if (todos[id-1]){
-        todos[id-1].title = req.body.title;
-        todos[id-1].price = req.body.price;
-        todos[id-1].thumbnail = req.body.thumbnail;
-        productos.guardarProductos(todos)
-        
-        return res.status(200).json(`producto con id ${id} modificado`)
-    }else{
-        return res.status(400).json(`No hay productos con id ${id}`)
-    }
+routerProductos.put("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const todos = await productos.getAll();
+  if (todos[id - 1]) {
+    todos[id - 1].title = req.body.title;
+    todos[id - 1].price = req.body.price;
+    todos[id - 1].thumbnail = req.body.thumbnail;
+    productos.guardarProductos(todos);
 
-})
+    return res.status(200).json(`producto con id ${id} modificado`);
+  } else {
+    return res.status(400).json(`No hay productos con id ${id}`);
+  }
+});
 
-
-routerProductos.delete("/:id", async(req, res)=>{
-    const id = Number(req.params.id)
-    await productos.deleteByID(id)
-    return res.status(200).json({})
-    
-})
+routerProductos.delete("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  await productos.deleteByID(id);
+  return res.status(200).json({});
+});
 module.exports = routerProductos;
--- clase8/server.js
+++ clase8/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+
 const routerProductos = require("./routes/routesProductos");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use("/", express.static(__dirname + "/public"));
+
 
 app.use("/api/productos", routerProductos);
 
