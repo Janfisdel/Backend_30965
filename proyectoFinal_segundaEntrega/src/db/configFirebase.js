59379f38b6d14b504ccafd172b8db6c3c24490e5
Jana Fisdel <janafisdel@gmail.com>
2022-07-05 18:33:10 -0300

    configuracion de DB en comentario

--- proyectoFinal_segundaEntrega/src/db/configFirebase.js
+++ proyectoFinal_segundaEntrega/src/db/configFirebase.js
@@ -1,15 +1,15 @@
-// db/configFirebase.js
-var admin = require("firebase-admin");
+// // db/configFirebase.js
+// var admin = require("firebase-admin");
 
-var serviceAccount = require('./serviceAccountKey.json');
+// var serviceAccount = require('./serviceAccountKey.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL:'https://project-1484720104025444351.firebaseio.com'
-})
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL:'https://project-1484720104025444351.firebaseio.com'
+// })
 
-const db = admin.firestore()
-const queryProductos = db.collection('productos')
-const queryCarritos = db.collection('carritos')
+// const db = admin.firestore()
+// const queryProductos = db.collection('productos')
+// const queryCarritos = db.collection('carritos')
 
-module.exports={queryProductos,queryCarritos}
+// module.exports={queryProductos,queryCarritos}
